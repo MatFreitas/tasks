2cf05fd2e8f0ec77ca7a56c071cded0c87572eb2
MatFreitas <matheusfs2@al.insper.edu.br>
2021-12-01 15:31:33 -0300

    correção taskview

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,8 +3,6 @@ from . import views
 from django.urls import include, path
 from rest_framework import routers
 
-router = routers.DefaultRouter()
-router.register(r'tasks', views.TaskViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
